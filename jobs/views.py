bef26fbdc5cf336c557db16376fe824c5cea697b
Alexandr Kroshko <akroshko1995@gmail.com>
2022-06-10 11:59:10 +0300

    sth

--- jobs/views.py
+++ jobs/views.py
@@ -5,12 +5,13 @@ from .forms import PositionForm, EmployeeForm
 
 
 def index(request):
-    employee_list = Employee.objects.raw(
-        'SELECT * FROM "jobs_employee" LEFT OUTER JOIN "jobs_position" ON'
-        ' ("jobs_employee"."position_id" = "jobs_position"."id") ORDER BY '
-        '"jobs_employee"."last_name" ASC, "jobs_employee"."first_name" ASC, '
-        '"jobs_employee"."patronymic" ASC'
-    )
+    # employee_list = Employee.objects.raw(
+    #     'SELECT * FROM "jobs_employee" LEFT OUTER JOIN "jobs_position" ON'
+    #     ' ("jobs_employee"."position_id" = "jobs_position"."id") ORDER BY '
+    #     '"jobs_employee"."last_name" ASC, "jobs_employee"."first_name" ASC, '
+    #     '"jobs_employee"."patronymic" ASC'
+    # )
+    employee_list = Employee.objects.select_related('position')
     form = EmployeeForm()
     # positions_list = Position.objects.all()
     context = {'employee_list': employee_list, 'form': form}
